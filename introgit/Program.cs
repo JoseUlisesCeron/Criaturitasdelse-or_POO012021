348a8bc78066ddb8af8ef6fa26b83e69c820babb
JoseUlisesCeron <00164319@uca.edu.sv>
2021-03-15 13:16:27 -0600

    end rama02

--- introgit/Program.cs
+++ introgit/Program.cs
@@ -9,18 +9,18 @@ namespace Labwork01
             //DECLARAMOS LAS VARIABLES 
             int num1 = 0, num2 = 0;
 
-            Console.WriteLine("Digite el primer numero a sumar: ");
+            Console.WriteLine("Digite el primer numero para multiplicar: ");
             num1 =  Int32.Parse (Console.ReadLine());
 
-            Console.WriteLine("Digite el segundo numero a sumar: ");
+            Console.WriteLine("Digite el segundo numero para multipolicargit ad: ");
             num2 = Int32.Parse (Console.ReadLine());
             //sumamos las variables 
-             Console.WriteLine($"sumando {num1} + {num2} igual {AddNumbers(num1, num2)}" );
+             Console.WriteLine($"multiplicando {num1} * {num2} igual {AddNumbers(num1, num2)}" );
 
         }
             public static int AddNumbers(int num1, int num2)
             {
-              return num1 + num2;
+              return num1 * num2;
             }
 
     }
