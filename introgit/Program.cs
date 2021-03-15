ca52bdabd135c5a2ce44ecf79f84822c0d18b162
rodriguez589 <00108720@uca.edu.sv>
2021-03-15 13:27:24 -0600

    a subtraction was made in the program

--- introgit/Program.cs
+++ introgit/Program.cs
@@ -9,18 +9,18 @@ namespace Labwork01
             //DECLARAMOS LAS VARIABLES 
             int num1 = 0, num2 = 0;
 
-            Console.WriteLine("Digite el primer numero a sumar: ");
+            Console.WriteLine("Digite el primer numero a restar: ");
             num1 =  Int32.Parse (Console.ReadLine());
 
-            Console.WriteLine("Digite el segundo numero a sumar: ");
+            Console.WriteLine("Digite el segundo numero a restar: ");
             num2 = Int32.Parse (Console.ReadLine());
             //sumamos las variables 
-             Console.WriteLine($"sumando {num1} + {num2} igual {AddNumbers(num1, num2)}" );
+             Console.WriteLine($"restando {num1} - {num2} igual {AddNumbers(num1, num2)}" );
 
         }
             public static int AddNumbers(int num1, int num2)
             {
-              return num1 + num2;
+              return num1 - num2;
             }
 
     }
